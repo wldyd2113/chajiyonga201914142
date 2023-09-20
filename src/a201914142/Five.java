9734e17a45e6db8708044192cfd1382ad61a7926
Cha, Jiyong <wldyd2113@gmail.com>
2023-09-20 11:09:33 +0900

    five

---
+++ src/a201914142/Five.java
@@ -0,0 +1,8 @@
+package a201914142;
+
+public class Five {
+
+	public static void print() { System.out.println("five");}
+	
+
+}
--- src/a201914142/Main.java
+++ src/a201914142/Main.java
@@ -2,7 +2,7 @@ package a201914142;
 
 public class Main {
 	public static void main(String[] args) {
-		Two.print();
+		Five.print(); Two.print();
 		System.out.println("one 201914142"); 
 		Three.print();
 		}
