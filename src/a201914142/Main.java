9694ed94a554cf5104fd0094c9e9013cc707cf63
Cha, Jiyong <wldyd2113@gmail.com>
2023-09-20 10:54:07 +0900

    three

--- src/a201914142/Main.java
+++ src/a201914142/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Two.print();
 		System.out.println("one 201914142"); 
+		Three.print();
 		}
 
 }
---
+++ src/a201914142/Three.java
@@ -0,0 +1,6 @@
+package a201914142;
+
+public class Three {
+
+	public static void print() { System.out.println("three"); }
+}
